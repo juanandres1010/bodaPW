74f9edd4880978ac22c74946b69964cadc8f1129
jhadderx <piromanojhadder@gmail.com>
2025-01-08 02:01:20 -0400

    Primer commit con mi proyecto de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bodaProject';
+
+  isPlaying = false;
+  currentTime = '0:00';
+  duration = '0:00';
+
+  ngOnInit(): void { }
+
+  togglePlayPause(audio: HTMLAudioElement): void {
+    if (audio.paused) {
+      audio.play();
+      this.isPlaying = true;
+      this.updateCurrentTime(audio);
+    } else {
+      audio.pause();
+      this.isPlaying = false;
+    }
+  }
+
+  updateCurrentTime(audio: HTMLAudioElement): void {
+    const interval = setInterval(() => {
+      if (!audio.paused) {
+        this.currentTime = this.formatTime(audio.currentTime);
+        this.duration = this.formatTime(audio.duration);
+      } else {
+        clearInterval(interval);
+      }
+    }, 500);
+  }
+
+  formatTime(time: number): string {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60).toString().padStart(2, '0');
+    return `${minutes}:${seconds}`;
+  }
 }
